ec7c3aed402884ce301d04a47d2948eb88494ec3
Chris <cm.hobbs94@gmail.com>
2021-03-22 21:13:54 +1000

    WIP: Chat Server basic functionality. Requires error handling, client clean disconnect, etc.

--- chat-server.cpp
+++ chat-server.cpp
@@ -7,7 +7,7 @@ namespace io = boost::asio;
 using tcp = io::ip::tcp;
 using error_code = boost::system::error_code;
 
-using message_handler = std::function<void (std::string)>;
+using message_handler = std::function<void (std::string message)>;
 using error_handler = std::function<void ()>;
 
 class Session : public std::enable_shared_from_this<Session>
@@ -19,22 +19,62 @@ class Session : public std::enable_shared_from_this<Session>
         {
         }
 
-        void Start(message_handler&& on_message, error_code&& on_error)
+        void Start(size_t client_id,message_handler&& on_message)
         {
+            m_on_message = on_message;
+            m_client_id = ++client_id;
+            AsyncRead();
+        }
 
+        void AsyncRead()
+        {
             boost::asio::async_read_until(m_socket, m_stream_buffer, '\n',boost::bind(&Session::HandleInput,shared_from_this(),boost::asio::placeholders::error,boost::asio::placeholders::bytes_transferred));
 
         }
 
         void HandleInput(boost::system::error_code error, std::size_t bytes_transferred)
         {
-            std::istream is(&m_stream_buffer);
-            std::cout << is.rdbuf();
+            if(!error) {
+                std::istream is(&m_stream_buffer);
+                std::stringstream sstream;
+
+                sstream << "Client " << m_client_id << ": " << is.rdbuf();
+
+                std::cout << sstream.str();
+
+                m_on_message(sstream.str());
+            }
+
+            AsyncRead();
         }
 
-        void Post() 
+        void Post(std::string& message) 
         {
-            
+            bool idle = m_outgoing_messages.empty();
+            m_outgoing_messages.push(message);
+
+            if(idle)
+            {
+                AsyncWrite();
+            }
+        }
+
+        void AsyncWrite()
+        {
+            io::async_write(m_socket,io::buffer(m_outgoing_messages.front()),std::bind(&Session::OnWrite,shared_from_this(),std::placeholders::_1,std::placeholders::_2));
+        }
+
+        void OnWrite(boost::system::error_code error, std::size_t bytes_transferred) {
+            if(!error) {
+
+                m_outgoing_messages.pop();
+
+                if(!m_outgoing_messages.empty()) {
+                    AsyncWrite();
+                } 
+            } else {
+                m_socket.close(error);
+            }
         }
 
     public:
@@ -44,8 +84,9 @@ class Session : public std::enable_shared_from_this<Session>
         tcp::socket m_socket;
         boost::asio::streambuf m_stream_buffer;
         std::queue<std::string> m_outgoing_messages;
-        message_handler on_message;
-        error_handler on_error;
+        message_handler m_on_message;
+        error_handler m_on_error;
+        size_t m_client_id;
 };
 
 class Server
@@ -55,36 +96,42 @@ class Server
             : m_io_context { io_context }
             , m_acceptor {m_io_context,tcp::endpoint(tcp::v4(),port)}
             , m_clients {}
+            , m_new_session {}
         {
-
         }
 
         void AcceptAsync() 
         {
-            std::shared_ptr<Session> new_session { std::make_shared<Session>(m_io_context) };
-            m_acceptor.async_accept(new_session->GetSocket(),boost::bind(&Server::OnConnection,this,boost::placeholders::_1));
+            m_new_session = std::make_shared<Session>(m_io_context);
+            m_acceptor.async_accept(m_new_session->GetSocket(),std::bind(&Server::OnConnection,this));
         }
 
-        void OnConnection(std::shared_ptr<Session> new_session)
+        void OnConnection()
         {
-            new_session->Start(Post,RemoveClient);
+            m_new_session->Start(m_clients.size(),std::bind(&Server::Post,this,std::placeholders::_1));
+            m_clients.push_back(m_new_session);
+
+            std::stringstream is;
+            is << "Server: Received Client " << m_clients.size() << "\n\r";
+
+            Post(is.str());
             AcceptAsync();
         }
 
-        void Post()
+        void Post(std::string message)
         {
-
+            for(auto &client : m_clients) {
+                client->Post(message);
+            }
         }
 
-        void RemoveClient()
-        {
-
-        }
+        
 
     private:
         boost::asio::io_context& m_io_context;
         tcp::acceptor m_acceptor;
         std::vector<std::shared_ptr<Session>> m_clients;
+        std::shared_ptr<Session> m_new_session;
 };
 
 
